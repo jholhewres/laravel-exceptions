e98969a430e5fec90237fb33573cb73145bd9051
Jhol Hewres <jhol.hewres@acesseseucondominio.com>
2024-02-08 21:00:33 -0300

    add internal error

--- app/Exceptions/CustomException.php
+++ app/Exceptions/CustomException.php
@@ -6,5 +6,8 @@ use Exception;
 
 class CustomException extends Exception
 {
-    //
+    public static function internalException(): static
+    {
+        return new static('Internal error', 500);
+    }
 }
